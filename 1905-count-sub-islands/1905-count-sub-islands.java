4a2d37c8e7fab4fd0959449e5fd5b8f8bebdf038
ritika8233 <64962761+ritika8233@users.noreply.github.com>
2023-04-13 13:09:17 +0530

    Time: 27 ms (53.76%), Space: 73.7 MB (43.79%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,29 +1,10 @@
-class Solution {
-    
-    public boolean util(int i, int j, int r, int c, int[][] arr){
-        
-        if(i < 0 || j < 0 || i >= r || j >= c) return true;
-        
-        if(arr[i][j] == 0 || arr[i][j] == 2) return true;
-        
-        arr[i][j] = 2;
-        
-        boolean left = false, right = false, top = false, bottom = false;
-        
-        left = util(i, j-1, r, c, arr);
-        right = util(i, j+1, r, c, arr);
-        top = util(i-1, j, r, c, arr);
-        bottom = util(i+1, j, r, c, arr);
-        
-        return left && right && top && bottom;
-    }
-    
+class Solution {    
     
     public boolean check(int i, int j, int r, int c, int[][] arr2, int[][] arr1){
         
         if(i < 0 || j < 0 || i >= r || j >= c) return true;
         
-        if(arr1[i][j] != 2  && arr2[i][j] == 1) return false;
+        if(arr1[i][j] != 1  && arr2[i][j] == 1) return false;
         
         if(arr2[i][j] == 0) return true;
         
@@ -45,15 +26,7 @@ class Solution {
         
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
-                if(arr1[i][j] == 1){
-                    util(i, j, r, c, arr1);
-                }
-            }
-        }
-        
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                if(arr2[i][j] == 1 && arr1[i][j] == 2){
+                if(arr2[i][j] == 1 && arr1[i][j] == 1){
                     // System.out.println(i + "-" + j + "=" + ans);
                     if(check(i, j, r, c, arr2, arr1)){
                         ans++;

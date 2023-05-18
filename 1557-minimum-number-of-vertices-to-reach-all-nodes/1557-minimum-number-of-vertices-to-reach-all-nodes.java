9e648e1f84f61e3559da32d868407c9c87598ccb
ritika8233 <64962761+ritika8233@users.noreply.github.com>
2023-05-18 22:17:26 +0530

    Time: 31 ms (15.67%), Space: 89.4 MB (32.62%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,22 +1,8 @@
 class Solution {
-    
-    public void util(int i, int n, List<List<Integer>> adj, int[] vis){
-        
-        if(i >= n)  return;
-        
-        
-        for(int itr : adj.get(i)){
-            if(vis[itr] == 0){
-                vis[itr] = 1;
-                util(itr, n, adj, vis);
-            }
-        }
-        
-    }
-    
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
         
         List<Integer> ans = new ArrayList<>();
+        int m = edges.size();
         
         List<List<Integer>> adj = new ArrayList<>();
         
@@ -24,21 +10,19 @@ class Solution {
             adj.add(new ArrayList<>());
         }
         
-        for(int i = 0; i < edges.size(); i++){
-            adj.get(edges.get(i).get(0)).add(edges.get(i).get(1));
-        }
+        int[] incoming = new int[n];
         
-        int[] vis = new int[n];
+        for(int i = 0; i < m; i++){
+            int u = edges.get(i).get(0);
+            int v = edges.get(i).get(1);
+            adj.get(u).add(v);
+            incoming[v]++;
+        }
         
         for(int i = 0; i < n; i++){
-            
-            util(i, n, adj, vis);
-            
+            if(incoming[i] == 0) ans.add(i);
         }
         
-        for(int  i = 0; i < n; i++){
-            if(vis[i] == 0) ans.add(i);
-        }
         
         return ans;
     }

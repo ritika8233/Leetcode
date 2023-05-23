346c43e37b5a355e7a35d73e58983cea3580f870
ritika8233 <64962761+ritika8233@users.noreply.github.com>
2023-05-23 11:39:25 +0530

    Time: 1354 ms (10.07%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,13 @@
+select res.product_id, res.store, res.price
+from (
+    select product_id, "store1" as "store", store1 as price
+    from Products where store1 is not null
+    UNION
+    select product_id, "store2" as "store", store2 as price
+    from Products where store2 is not null
+    UNION
+    select product_id, "store3" as "store", store3 as price
+    from Products where store3 is not null
+    ) as res
+order by res.product_id
+;
